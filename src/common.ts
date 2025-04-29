4d6be0ba2bc803c07241d1efd77ba98a1d13afa6
Fabian Morón Zirfas <hash@ff6347.email>
2025-04-29 07:49:00 +0200

    fix: Type imports separate from normal imports
    Makes tests happier

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,5 @@
-import { SupabaseClient } from "@supabase/supabase-js";
-import { Database } from "./database.ts";
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "./database.ts";
 
 export interface ExtractedLink {
 	url: string;
